ecea96a6fc2ea6ffbcb997f62ab2ae052b7ed25c
haopo <haopoboy@gmail.com>
2020-08-03 21:15:22 +0800

    2 Changes: * Use page as default data of response * Provide apiBasePath for services

--- src/app/menu/menu.service.ts
+++ src/app/menu/menu.service.ts
@@ -1,20 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { environment } from "src/environments/environment";
+import { Page } from "../service/Page";
 
 @Injectable({
   providedIn: "root",
 })
 export class MenuService {
+  url = `${environment.apiBasePath}/menuItems`;
   items$: Observable<MenuItem[]> = this.http
-    .get<MenuItem[]>("api/menuItems")
-    .pipe();
+    .get<Page<MenuItem>>(this.url)
+    .pipe(map((page) => page.content));
 
   constructor(private http: HttpClient) {}
-
-  findAll(): Observable<MenuItem[]> {
-    return this.http.get<MenuItem[]>("/api/menuItems");
-  }
 }
 
 export interface MenuItem {
--- src/app/service/app-in-memory-db.service.ts
+++ src/app/service/app-in-memory-db.service.ts
@@ -1,25 +1,80 @@
 import { Injectable } from "@angular/core";
-import { InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
+import {
+  InMemoryDbService,
+  RequestInfo,
+  ResponseOptions,
+} from "angular-in-memory-web-api";
 import { Observable } from "rxjs";
 import { MenuItem } from "../menu/menu.service";
+import { Link } from "./../search/search.service";
+import { PageImpl } from "./Page";
 
 @Injectable()
 export class AppInMemoryDbService implements InMemoryDbService {
   constructor() {}
+
   createDb(reqInfo?: RequestInfo): {} | Observable<{}> | Promise<{}> {
-    return { menuItems: this.menuItems() };
+    return { menuItems: this.menuItems(), links: this.links() };
+  }
+
+  responseInterceptor(
+    response: ResponseOptions,
+    requestInfo: RequestInfo
+  ): ResponseOptions {
+    if ("post" === requestInfo.method) {
+      // Fix undefine body from request when updated data
+      response.body = response.body
+        ? response.body
+        : (requestInfo.req as any).body;
+      return response;
+    } else if (requestInfo.id) {
+      return response;
+    } else {
+      // United pageable from server
+      const page = PageImpl.of(response.body);
+      response.body = page;
+      return response;
+    }
   }
 
   menuItems(): MenuItem[] {
     const data = [
-      { id: 0, icon: "dashboard", label: "Dashboard", routerLink: "dashboard" },
+      {
+        id: 0,
+        icon: "dashboard",
+        label: "Dashboard",
+        routerLink: "/dashboard",
+      },
       {
         id: 0,
         icon: "description",
         label: "Documents",
-        routerLink: "documents",
+        routerLink: "/documents",
       },
     ];
+    this.assignId(data);
+    return data;
+  }
+
+  links(): Link[] {
+    let data: Link[] = [
+      {
+        label: "Default",
+        keyword: "default",
+      },
+    ];
+    data = data.concat(
+      this.menuItems().map((item) => {
+        return {
+          label: item.label,
+          routerLink: item.routerLink,
+          keyword: item.label,
+          type: "menu",
+          data: item,
+        };
+      })
+    );
+
     this.assignId(data);
     return data;
   }
