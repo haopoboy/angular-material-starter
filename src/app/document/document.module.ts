7bf53d6a6bb3cecde6ca624f3f2f1c28dadd6196
haopo <haopoboy@gmail.com>
2020-09-04 18:28:10 +0800

    Show markdown content by ngx-markdown

--- src/app/document/document.module.ts
+++ src/app/document/document.module.ts
@@ -2,8 +2,10 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
+import { MarkdownModule } from "ngx-markdown";
 import { DocumentRoutingModule } from "./document-routing.module";
 import { DocumentComponent } from "./document.component";
 import { DocumentsComponent } from "./documents/documents.component";
@@ -17,6 +19,8 @@ import { DocumentsComponent } from "./documents/documents.component";
     MatCardModule,
     MatIconModule,
     MatButtonModule,
+    MatDividerModule,
+    MarkdownModule.forRoot(),
   ],
 })
 export class DocumentModule {}
